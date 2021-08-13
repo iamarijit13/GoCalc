a73e40ead9b9b5d6f4e89fa0bf92f97dbe4ba2c6
ARIJIT <68080361+arispeak@users.noreply.github.com>
2021-08-13 11:10:24 +0530

    Normal Calculation functions.

---
+++ calc.go
@@ -0,0 +1,17 @@
+package calc
+
+func Add (first int, second int) int {
+	return first + second;
+}
+
+func Sub (first int, second int) int {
+	return first - second;
+}
+
+func Mul (first int, second int) int {
+	return first * second;
+}
+
+func Div (first int, second int) int {
+	return first / second;
+}
